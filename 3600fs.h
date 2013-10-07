703b6c35f7825673af33d289b4c7b50275fdc1c6
Nathan VanBenschoten <nvanbenschoten@gmail.com>
2013-10-07 08:47:13 -0400

    File didnt get saved before last commit

--- 3600fs.h
+++ 3600fs.h
@@ -120,6 +120,6 @@ int bufdwrite(int blocknum, const char * buf, int size);
 // File system specific helper functions
 int seperatePathAndName(char *path, char *name);
 int findDNODE(dnode *directory, char *path);
-int getNODE(dnode *directory, dnode *searchDnode, inode *searchInode);
+int getNODE(dnode *directory, char *name, dnode *searchDnode, inode *searchInode);
 
 #endif
